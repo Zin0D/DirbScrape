70ce906a28a51d5b82ff3adc727ec6e53eaf8b03
Zin <zinedinemokhefi@gmail.com>
2024-12-05 22:46:27 +0100

    Removed uneccesary files, for the 4th time...

--- FuckGithub/OStest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-PROXIE = { #Configuring the Tor Proxy.
-    "http" : "socks5://127.0.0.1:9050",
-    "https" : "socks5://127.0.0.1:9050"
-}
-x = requests.get("https://google.com",proxies=PROXIE,timeout=10)
